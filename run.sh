a0cfa508a4a45ee136cbfadf6afa4520b20696f1
Ubuntu <you@example.com>
2020-12-06 01:25:21 +0000

    Update

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#docker build -t wn1980/raspap
+
+docker run --name raspap -it -d \
+    --privileged \
+    --network=host \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+    --cap-add SYS_ADMIN \
+    wn1980/raspap
+
+#docker exec -it raspap bash
+#$ ./setup.sh
+#docker restart raspap
+#access WEB-UI at http://localhost
+
